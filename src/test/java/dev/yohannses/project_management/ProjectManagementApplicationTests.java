256f5be3984408ec3f8ed782676bda51a9a22cec
Yohannes Terfa <yohannes.terfa@safaricom.et>
2025-10-28 15:11:13 +0300

    feat: add active profile configuration and enhance test documentation

--- src/test/java/dev/yohannses/project_management/ProjectManagementApplicationTests.java
+++ src/test/java/dev/yohannses/project_management/ProjectManagementApplicationTests.java
@@ -2,12 +2,17 @@ package dev.yohannses.project_management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 
 @SpringBootTest
 class ProjectManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
+        ApplicationModules modules = ApplicationModules.of(ProjectManagementApplication.class);
+        modules.verify();
+        new Documenter(modules).writeDocumentation().writeIndividualModulesAsPlantUml();
 	}
 
 }

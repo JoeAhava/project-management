67bbe4b1f325badb176b8fddb1a18da576e42b8c
Yohannes Terfa <yohannes.terfa@safaricom.et>
2025-11-11 16:19:47 +0300

    feat(projects): create Project on ProposalCreatedEvent
    - ProjectService listens to ProposalCreatedEvent and creates Project from Proposal

--- src/main/java/dev/yohannses/project_management/projects/internal/ProjectService.java
+++ src/main/java/dev/yohannses/project_management/projects/internal/ProjectService.java
@@ -6,13 +6,38 @@ import org.slf4j.LoggerFactory;
 import org.springframework.modulith.events.ApplicationModuleListener;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 public class ProjectService {
 
     private static final Logger LOG = LoggerFactory.getLogger(ProjectService.class);
+    private final ProjectRepository repo;
+
+    public ProjectService(ProjectRepository repo) {
+        this.repo = repo;
+    }
 
     @ApplicationModuleListener
     void proposalCreated(ProposalCreatedEvent event) {
-        LOG.info("✅ Project Created for New Proposal with ID: {} and payload: {}", event.id(), event.proposal());
+        Optional<Project> optionalProject = repo.findByProposalId(event.id());
+        optionalProject.ifPresent(project -> LOG.warn("⛔ Project exists for proposal with ID: {} and payload: {}", event.id(), project));
+
+        LOG.info("🔃 Project not found creating");
+
+        Project newProject = Project.builder()
+                .title(event.proposal().getTitle())
+                .description(event.proposal().getDescription())
+                .detailedDescription(event.proposal().getDetailedDescription())
+                .proposalId(event.id())
+                .status(ProjectStatus.ACTIVE)
+                .build();
+
+        Project createdProject = repo.save(newProject);
+
+        LOG.info("✅ Project Created for New Proposal with ID: {} and payload: {}", event.id(), createdProject);
     }
 }
